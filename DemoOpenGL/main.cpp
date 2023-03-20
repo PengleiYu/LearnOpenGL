f1c56c25b98b48e4eabba524e4f9a70d11eaffe6
yupenglei <yupenglei@kuaishou.com>
2023-03-20 14:57:02 +0800

    Initial Commit

---
+++ DemoOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DemoOpenGL
+//
+//  Created by yupenglei on 2023/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
