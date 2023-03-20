383dd58a39cdcfc6f49a2125f0916c3beb802a79
yupenglei <yupenglei@kuaishou.com>
2023-03-20 17:08:25 +0800

    init，第一个程序

--- DemoOpenGL/main.cpp
+++ DemoOpenGL/main.cpp
@@ -5,10 +5,80 @@
 //  Created by yupenglei on 2023/3/20.
 //
 
+//#include <iostream>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+// 阻止警告信息生成
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+
+
+// HomeBrew将GLEW安装为静态库
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+void init(GLFWwindow* window){}
+
+void display(GLFWwindow* window, double currentTime){
+    glClearColor(1.0, 0.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(void){
+    // 初始化glfw
+    if(!glfwInit()){
+        exit(EXIT_FAILURE);
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    // -mac下openGL为4.1
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    
+    // -强制使用硬件支持的最新OpenGL版本，很多MAC默认使用老版本的OpenGL
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Hello OpenGL", NULL, NULL);
+    
+    // -获取窗口分辨率（针对视网膜屏MAC）
+    int actualScreenWidth, actualScreenHeight;
+    glfwGetFramebufferSize(window, &actualScreenWidth, &actualScreenHeight);
+    
+    // 绑定window和openGL上下文，创建窗口并不会自动绑定上下文
+    glfwMakeContextCurrent(window);
+    
+    // -确保绘制到帧缓冲区的内容与窗口匹配（针对视网膜屏MAC）
+    glViewport(0,0,actualScreenWidth, actualScreenHeight);
+    glewExperimental = GL_TRUE;
+    
+    // 初始化glew
+    if(glewInit()!=GLEW_OK){
+        exit(EXIT_FAILURE);
+    }
+    // 开启垂直同步，GLFW窗口默认双缓冲
+    glfwSwapInterval(1);
+    
+    init(window);
+    
+    while (!glfwWindowShouldClose(window)) {
+        // 自定义渲染函数
+        display(window, glfwGetTime());
+        // 绘制屏幕
+        glfwSwapBuffers(window);
+        // 处理窗口相关事件
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
