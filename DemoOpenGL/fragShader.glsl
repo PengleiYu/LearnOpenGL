5e76b79cbb8df1536385bec0979b1664d6be5e23
yupenglei <yupengniaosanshi@gmail.com>
2023-04-01 01:13:18 +0800

    实现红色正方体

--- DemoOpenGL/fragShader.glsl
+++ DemoOpenGL/fragShader.glsl
@@ -1,8 +1,9 @@
 #version 410
 out vec4 color;
-void main(void) {
-    if(gl_FragCoord.x<295)
-        color = vec4(0.0,1.0,0.0,1.0);
-    else
-        color=vec4(0.0,0.0,1.0,1.0);
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+
+void main(void)
+{    color = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- DemoOpenGL/main.cpp
+++ DemoOpenGL/main.cpp
@@ -30,39 +30,79 @@
 using namespace std;
 
 #define numVAOs 1
-
+#define numVBOs 2
+Utils util = Utils();
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
-GLuint offsetLoc;
-float x = 0.0f;// 三角形x轴坐标
-float inc = 0.01f;// 三角形移动距离
+GLuint vbo[numVBOs];
 
-void init(GLFWwindow* window){
-    renderingProgram = Utils::createShaderProgram("vertShader.glsl","fragShader.glsl");
-    // 创建顶点数组缓冲区
-    glGenVertexArrays(numVAOs,vao);
+// variable allocation for display
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+
+void setupVertices(void) {
+    float vertexPositions[108] = {
+        -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+    };
+
+    glGenVertexArrays(1, vao);
     glBindVertexArray(vao[0]);
+    glGenBuffers(numVBOs, vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 }
-void display(GLFWwindow* window, double currentTime){
+
+void init(GLFWwindow* window) {
+    renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+    cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+    setupVertices();
+}
+
+void display(GLFWwindow* window, double currentTime) {
     glClear(GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    // 运行着色器程序
+
     glUseProgram(renderingProgram);
-    
-    x += inc;
-    if (x>1.0f) {
-        inc *= -1;
-    }else if (x<-1.0f){
-        inc *= -1;
-    }
-    // 获取指向offset的指针
-    offsetLoc=glGetUniformLocation(renderingProgram,"offset");
-    // 将x赋值给offset
-    glProgramUniform1f(renderingProgram,offsetLoc,x);
-    
-    // 绘制缓冲区内容
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+    glfwGetFramebufferSize(window, &width, &height);
+    aspect = (float)width / (float)height;
+    pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+    mvMat = vMat * mMat;
+
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
 int main(void){
