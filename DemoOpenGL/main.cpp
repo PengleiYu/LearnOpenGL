fd1e498d6279e721dd22a9cc3a02bdcce273f62b
yupenglei <yupengniaosanshi@gmail.com>
2023-04-01 22:38:15 +0800

    绘制多个移动立方体

--- DemoOpenGL/main.cpp
+++ DemoOpenGL/main.cpp
@@ -60,11 +60,11 @@ void setupVertices(void) {
         -1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
         1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
     };
-
+    
     glGenVertexArrays(1, vao);
     glBindVertexArray(vao[0]);
     glGenBuffers(numVBOs, vbo);
-
+    
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 }
@@ -79,55 +79,59 @@ void init(GLFWwindow* window) {
 void display(GLFWwindow* window, double currentTime) {
     glClear(GL_DEPTH_BUFFER_BIT);
     glClear(GL_COLOR_BUFFER_BIT);
-
+    
     glUseProgram(renderingProgram);
-
+    
     mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
     projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-
+    
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
-
+    
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
     
-    // 根据时间移动不同距离
-    tMat = glm::translate(glm::mat4(1.0f),
-                          glm::vec3(
-                                    sin(0.35f*currentTime),
-                                    cos(0.52f*currentTime),
-                                    sin(0.7f*currentTime))*2.0f
-                          );
-    // 根据时间旋转不同角度
-    float rSpeed = 1.75f;
-    rMat = glm::rotate(glm::mat4(1.0f),
-                       rSpeed*(float)currentTime,
-                       glm::vec3(0.0f, 1.0f, 0.0f)
-                       );
-    rMat = glm::rotate(rMat,
-                       rSpeed*(float)currentTime,
-                       glm::vec3(1.0f,0.0f,0.0f)
-                       );
-    rMat = glm::rotate(rMat,
-                       rSpeed*(float)currentTime,
-                       glm::vec3(0.0f,0.0f,1.0f)
-                       );
-    
-//    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
-    mMat = tMat * rMat;
-    mvMat = vMat * mMat;
-
-    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-    glEnableVertexAttribArray(0);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    double finalTime = currentTime;
+    for(int i=0;i<24;i++){
+        currentTime = finalTime + i;
+        // 根据时间移动不同距离
+        tMat = glm::translate(glm::mat4(1.0f),
+                              glm::vec3(
+                                        sin(0.35f*currentTime),
+                                        cos(0.52f*currentTime),
+                                        sin(0.7f*currentTime))*8.0f
+                              );
+        // 根据时间旋转不同角度
+        float rSpeed = 1.75f;
+        rMat = glm::rotate(glm::mat4(1.0f),
+                           rSpeed*(float)currentTime,
+                           glm::vec3(0.0f, 1.0f, 0.0f)
+                           );
+        rMat = glm::rotate(rMat,
+                           rSpeed*(float)currentTime,
+                           glm::vec3(1.0f,0.0f,0.0f)
+                           );
+        rMat = glm::rotate(rMat,
+                           rSpeed*(float)currentTime,
+                           glm::vec3(0.0f,0.0f,1.0f)
+                           );
+        
+        //    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+        mMat = tMat * rMat;
+        mvMat = vMat * mMat;
+        
+        glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+        
+        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        glEnableVertexAttribArray(0);
+        
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 }
 
 int main(void){
