cdfb7e6551a5edf41573f0bf8a049eabbe72a20f
yupenglei <yupengniaosanshi@gmail.com>
2023-04-14 01:31:10 +0800

    构建球体

--- DemoOpenGL/main.cpp
+++ DemoOpenGL/main.cpp
@@ -26,77 +26,62 @@
 #include <fstream>
 #include <sstream>
 #include "Utils.h"
+#include "Sphere.hpp"
 
 using namespace std;
 
 #define numVAOs 1
-#define numVBOs 2
-Utils util = Utils();
+#define numVBOs 3
+
 float cameraX, cameraY, cameraZ;
-float pyrLocX, pyrLocY, pyrLocZ;
+float sphLocX, sphLocY, sphLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
+GLuint earthTexture;
+float rotAmt = 0.0f;
+
 
 // variable allocation for display
 GLuint mvLoc, projLoc, sampLoc;
 int width, height;
 float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat;
-glm::mat4 tMat, rMat;
 
-GLuint brickTexture;
+Sphere mySphere = Sphere(48);
 
 void setupVertices(void) {
-    float pyramidPositions[54] = {
-        -1.0f, -1.0f, 1.0f,
-        1.0f, -1.0f, 1.0f,
-        0.0f, 1.0f, 0.0f,//前侧面
-        1.0f, -1.0f, 1.0f,
-        1.0f, -1.0f, -1.0f,
-        0.0f, 1.0f, 0.0f,//右侧面
-        1.0f, -1.0f, -1.0f,
-        -1.0f, -1.0f, -1.0f,
-        0.0f, 1.0f, 0.0f,//后侧面
-        -1.0f, -1.0f, -1.0f,
-        -1.0f, -1.0f, 1.0f,
-        0.0f, 1.0f, 0.0f,//左侧面
-        -1.0f, -1.0f, -1.0f,
-        1.0f, -1.0f, 1.0f,
-        -1.0f, -1.0f, 1.0f,//底面1
-        1.0f, -1.0f, 1.0f,
-        -1.0f, -1.0f, -1.0f,
-        1.0f, -1.0f, -1.0f//底面2
-        };
-    float textureCoordinates[36] = {
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.5f, 1.0f,//前侧面
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.5f, 1.0f,//右侧面
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.5f, 1.0f,//后侧面
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.5f, 1.0f,//左侧面
-        0.0f, 0.0f,
-        1.0f, 1.0f,
-        0.0f, 1.0f,//底面1
-        1.0f, 1.0f,
-        0.0f, 0.0f,
-        1.0f, 0.0f//底面2
-    };
+    std::vector<int> ind = mySphere.getIndices();
+    std::vector<glm::vec3> vert = mySphere.getVertices();
+    std::vector<glm::vec2> tex = mySphere.getTexCoords();
+    std::vector<glm::vec3> norm = mySphere.getNormals();
+    
+    std::vector<float> pvalues;
+    std::vector<float> tvalues;
+    std::vector<float> nvalues;
+    
+    int numIndices = mySphere.getNumIndices();
+    for (int i = 0; i < numIndices; i++) {
+        pvalues.push_back((vert[ind[i]]).x);
+        pvalues.push_back((vert[ind[i]]).y);
+        pvalues.push_back((vert[ind[i]]).z);
+        tvalues.push_back((tex[ind[i]]).s);
+        tvalues.push_back((tex[ind[i]]).t);
+        nvalues.push_back((norm[ind[i]]).x);
+        nvalues.push_back((norm[ind[i]]).y);
+        nvalues.push_back((norm[ind[i]]).z);
+    }
    
-    // 创建缓冲区对象
     glGenBuffers(numVBOs, vbo);
-    // 写入四棱锥
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);// 设为当前缓冲区
-    glBufferData(GL_ARRAY_BUFFER, sizeof(pyramidPositions), pyramidPositions, GL_STATIC_DRAW);// 顶点数据写入缓冲区
-    // 写入纹理
+    // 写入顶点数组
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, pvalues.size()*4, &pvalues[0], GL_STATIC_DRAW);
+    // 写入纹理数组
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(textureCoordinates), textureCoordinates, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, tvalues.size()*4, &tvalues[0], GL_STATIC_DRAW);
+    // 写入法向量数组
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+    glBufferData(GL_ARRAY_BUFFER, nvalues.size()*4, &nvalues[0], GL_STATIC_DRAW);
     
     // 创建顶点数组对象
     glGenVertexArrays(1, vao);
@@ -106,9 +91,9 @@ void setupVertices(void) {
 
 void init(GLFWwindow* window) {
     renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
-    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 4.0f;
-    pyrLocX = 0.0f; pyrLocY = 0.0f; pyrLocZ = 0.0f;
-    brickTexture = Utils::loadTexture("brick1.jpg");
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 2.0f;
+    sphLocX = 0.0f; sphLocY = 0.0f; sphLocZ = -1.0f;
+    earthTexture = Utils::loadTexture("earth.jpg");
 
     setupVertices();
 }
@@ -126,17 +111,16 @@ void display(GLFWwindow* window, double currentTime) {
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
     
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-    
-    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(pyrLocX, pyrLocY, pyrLocZ));
-
-    mMat = glm::rotate(mMat, -0.45f, glm::vec3(1.0f, 0.0f, 0.0f));
-    mMat = glm::rotate(mMat,  0.61f, glm::vec3(0.0f, 1.0f, 0.0f));
-    mMat = glm::rotate(mMat,  0.00f, glm::vec3(0.0f, 0.0f, 1.0f));
-    
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(sphLocX, sphLocY, sphLocZ));
+    mMat = glm::rotate(mMat, (float) currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
     mvMat = vMat * mMat;
 
     glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, earthTexture);
+    glUniform1i(sampLoc, 0);//绑定纹理，shader中不能使用binding布局
+    
     
     glBindBuffer(GL_ARRAY_BUFFER,vbo[0]);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
@@ -145,14 +129,10 @@ void display(GLFWwindow* window, double currentTime) {
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(1);
-    
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, brickTexture);
-    glUniform1i(sampLoc, 0);//绑定纹理，shader中不能使用binding布局
-    
+
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
-    glDrawArrays(GL_TRIANGLES, 0, 18);
+    glDrawArrays(GL_TRIANGLES, 0, mySphere.getNumIndices());
 }
 
 void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
